02d7e945ff3d2ba5047cc1a509a54d1b9d55301a
朱林忠 <wb_zhulinzhong@meituan.com>
2022-03-01 19:53:57 +0800

    project init

---
+++ blog.client/src/main/java/com/zlz/website/blog/client/DemoClient.java
@@ -0,0 +1,8 @@
+package com.zlz.website.blog.client;
+
+/**
+ * @author zhulinzhong
+ * @date 2022-03-01 19:52:04
+ */
+public class DemoClient {
+}
---
+++ blog.common/src/main/java/com/zlz/website/blog/common/DemoDO.java
@@ -0,0 +1,8 @@
+package com.zlz.website.blog.common;
+
+/**
+ * @author zhulinzhong
+ * @date 2022-03-01 19:52:19
+ */
+public class DemoDO {
+}
