1151674937df5a5c23f4fb136a025c35947e2a06
朱林忠 <wb_zhulinzhong@meituan.com>
2022-03-01 15:55:17 +0800

    init

---
+++ blog.server/src/main/java/com/zlz/website/blog/WebsiteBlogServerApplication.java
@@ -0,0 +1,17 @@
+package com.zlz.website.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot starter
+ * @author zhulinzhong
+ */
+@SpringBootApplication
+public class WebsiteBlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBlogServerApplication.class, args);
+    }
+
+}
